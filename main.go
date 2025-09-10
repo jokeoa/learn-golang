fcb2ce7625697ad0eac25d727cbfd64e010f45d7
Madiyar Sarbaev <itzjokeoa@gmail.com>
2025-09-10 16:17:04 +0500

    add fibonacci inplementation

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-func foo() (int, error) {
-	return 1, nil
-}
-func main() {
+import "fmt"
 
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		result := a
+		a, b = b, a+b
+		return result
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
 }
